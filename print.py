8e63d900da1f8c17a933d83e7eea6a161fe38cf5
MYXAMOR338 <silova155@gmail.com>
2024-03-15 17:14:00 +0300

    Standart logging start ++

--- print.py
+++ print.py
@@ -5,5 +5,9 @@ logging.basicConfig(filename='LOGS.log', level=logging.INFO, format='%(asctime)s
 
 def log_and_print(*messages):
     formatted_message = ' '.join(map(str, messages))
-    logging.info(formatted_message)  # Запись в файл журнала
-    print(formatted_message)  # Вывод на консоль
+    if 'ошибка' in formatted_message or 'Ошибка' in formatted_message or 'Error' in formatted_message:
+        logging.error(formatted_message)  # Запись в файл журнала
+        print(formatted_message)  # Вывод на консоль
+    else:
+        logging.info(formatted_message)  # Запись в файл журнала
+        print(formatted_message)  # Вывод на консоль
