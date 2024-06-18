f66f2d4837934805fb6b748c5c1c5117c93919f0
MYXAMOR338 <silova155@gmail.com>
2024-06-18 16:27:27 +0300

    Открытие окна о программе

---
+++ ui_about.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'about.ui'
+##
+## Created by: Qt User Interface Compiler version 6.6.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QLabel, QSizePolicy, QWidget)
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        if not Form.objectName():
+            Form.setObjectName(u"Form")
+        Form.resize(822, 451)
+        self.label = QLabel(Form)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(20, 30, 721, 361))
+
+        self.retranslateUi(Form)
+
+        QMetaObject.connectSlotsByName(Form)
+    # setupUi
+
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
+        self.label.setText(QCoreApplication.translate("Form", u"\u041f\u043e\u043c\u043e\u0449\u043d\u0438\u043a \u0432 vk \u0441 CHAT GPT, \u043a\u043e\u0442\u043e\u0440\u044b\u0439 \u0441\u043f\u043e\u0441\u043e\u0431\u0435\u043d \u0441\u0430\u043c\u043e\u0441\u0442\u043e\u044f\u0442\u0435\u043b\u044c\u043d\u043e \u043e\u0431\u0449\u0430\u0442\u044c\u0441\u044f \u0432 \u043c\u0435\u0441\u0441\u0435\u043d\u0434\u0436\u0435\u0440\u0435 vk \u0441 \u0437\u0430\u0434\u0430\u043d\u043d\u044b\u043c\u0438 \u043f\u0430\u0440\u0430\u043c\u0435\u0442\u0440\u0430\u043c\u0438.", None))
+    # retranslateUi
+
--- widget.py
+++ widget.py
@@ -1,18 +1,33 @@
 # widget.py
-from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLineEdit
+from PySide6.QtWidgets import QApplication, QWidget, QDialog
 from ui_form import Ui_Widget
+from ui_about import Ui_Form  # Импортируем UI для нового окна
 import sys
 import subprocess
+
+# Класс для нового окна "О программе"
+class AboutForm(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
 
 class Widget(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.ui = Ui_Widget()
         self.ui.setupUi(self)
-        self.ui.pushButton_2.clicked.connect(self.on_start_button_clicked)
+
+        # Подключаем кнопки к их функциям
+        self.ui.pushButton.clicked.connect(self.show_about_form)  # Открыть новое окно
+        self.ui.pushButton_2.clicked.connect(self.on_start_button_clicked)  # Запустить main.py
+
+    def show_about_form(self):
+        self.about_form = AboutForm(self)
+        self.about_form.exec()  # Открываем новое окно
 
     def on_start_button_clicked(self):
-        subprocess.Popen(["python", "main.py"])
+        subprocess.Popen(["python", "main.py"])  # Запускаем main.py
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
